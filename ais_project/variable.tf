e053ab7f98c0f783784710a6bbd9193b8d919f84
Uchenna Chibueze <emmachi72.ec@gmail.com>
2023-02-22 19:41:39 +0100

    Added AIS AKS Modules

--- ais_project/variable.tf
+++ ais_project/variable.tf
@@ -8,7 +8,7 @@ variable "state_sa_name" {}
 
 variable "container_name" {}
 
-variable "access_key" {}
+# variable "access_key" {}
 
 variable "net_plugin" {
     default = "azure"  #Options are "azure" or "kubenet"
